89f462c51724655e9051aa9680afd92909e68918
MumtahinaRahman <vanallope2015@gmail.com>
2022-09-09 22:15:32 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
-<<<<<<< HEAD
-import React from 'react';
-=======
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router } from 'react-router-dom';
->>>>>>> Navbar
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <h1> hello! </h1>
-=======
       <Router>
         <NavBar />
       </Router>
->>>>>>> Navbar
     </div>
   );
 }
