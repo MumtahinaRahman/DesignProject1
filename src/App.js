692bb4c5bf90c610ad19a888a29be80d9e4fa608
MumtahinaRahman <vanallope2015@gmail.com>
2022-10-08 11:17:25 +0600

    shouldn't have pushed in main branch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar'
 import HomePage from './components/HomePage';
 import Footer from './components/Footer/Footer';
--- src/Pages/Lessons.jsx
+++ src/Pages/Lessons.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import learn1 from '../images/learn1.jpg'
 
 function Lessons() {
   return (
@@ -8,7 +7,6 @@ function Lessons() {
       <h1>show yourself</h1>
       <h1>let it go</h1>
       <h1>in to the unknown</h1>
-      <img src={learn1}/>
     </div>
     
   )
