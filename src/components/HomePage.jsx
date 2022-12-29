19ddb6005def5bfb6f7643e2cdbacf591b6bba3b
yobars <hellobars19@gmail.com>
2022-12-30 04:27:09 +0600

    Linked signup button from homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,6 +4,7 @@ import About from './About';
 import Learn from './Learn/Learn';
 import Interpret from './Interpret/Interpret';
 import videoBG from '../videos/sign1.mp4';
+import {Link} from 'react-router-dom';
 
 const HomePage = () => {
   return (
@@ -17,7 +18,7 @@ const HomePage = () => {
                 <p> <h2> <span> <b> Sign</b></span>Us </h2></p>
                 <h4>A bridge between deaf-mute and us</h4>
                 <h6>LEARN BANGLA SIGN LANGUAGE</h6>
-                <button type="button" className="btn btn-outline-light sign-up">Sign up</button>
+                <Link to='/signup'><button type="button" className="btn btn-outline-light sign-up">Sign up</button></Link>
             </div>
         </div>
         <About />
