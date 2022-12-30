f0c4ecfd740ec480852177bb4a72dd852cdba86d
MumtahinaRahman <vanallope2015@gmail.com>
2022-12-30 10:35:46 +0600

    video doesn't show

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import HandPose from './Pages/HandPose/HandPose';
 import Login from './Pages/Login/Login';
 import Profile from './Pages/Profile/Profile';
 import Consonants from './Pages/Consonants/Consonants';
+import BasicWords from './Pages/BasicWords/BasicWords';
 
 // style={{backgroundImage:`url(${bg})`,  }}
 
@@ -29,6 +30,7 @@ function App() {
             <Route path="lessons/lesson" element={<Lesson />}/>
             <Route path="lessons/lesson/:id" element={<LessonID />}/>
             <Route path="Consonants/Consonants" element={<Consonants />}/>
+            <Route path="BasicWords/BasicWords" element={<BasicWords />}/>
             <Route path="tests" element={<Tests />}/>
             <Route path="quiz" element={<Quiz />}/>
             <Route path="tests/webcam" element={<HandPose />}/>
